5acc2a5259704139151c5f6bfaf4d5d56ad07b26
WinterCricket <hodhodhod2003@gmail.com>
2018-08-29 13:44:17 -0500

    add vars and strokestyle to func

--- animation.js
+++ animation.js
@@ -20,6 +20,19 @@ function getRandColor() {
 }
 
 
+
+function showSquare() {
+
+
+	var canvas = document.getElementById("whiteBoard");
+	var context = canvas.getContext("2d");
+	 var verPos = Math.floor((Math.random() * 199) + 1);
+	
+}
+
+
 document.getElementById('buttons').onclick=function() {
 	lightShow();
 }
+
+var candocument.getElementById("whiteBoard").onclick=function() {
