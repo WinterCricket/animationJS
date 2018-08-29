2466ea4d66c021907bf5c13b9f8bb83d58349014
WinterCricket <hodhodhod2003@gmail.com>
2018-08-29 13:19:57 -0500

    begin function for colors

---
+++ animation.js
@@ -0,0 +1,21 @@
+
+// function lightShow() {
+
+// 	var showtime = getElementById('whiteBoard').
+
+// 	displayIt = showtime.innerHTML
+// }
+
+
+
+var notRunning = true; //prevents double-run bug
+
+function getRandColor() {
+	var letters = '0123456789ABCDEF'.split('');
+	var color = "#";
+}
+
+
+document.getElementById('buttons').onclick=function() {
+	lightShow();
+}
